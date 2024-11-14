a0f30649e6aead5cd2d57028b1dd530b3cb62ffc
Florent Collot <florent.collot@ihu-liryc.fr>
2024-11-14 17:53:32 +0100

    foo

--- library/source/pyMedInria/export.h
+++ library/source/pyMedInria/export.h
@@ -2,7 +2,7 @@
 // License: BSD-3-Clause
 
 #ifdef WIN32
-    #ifdef med_cpp_api_EXPORTS
+    #ifdef pyMedInria_library_EXPORTS
         #define MED_EXPORT __declspec(dllexport)
     #else
         #define MED_EXPORT __declspec(dllimport)
